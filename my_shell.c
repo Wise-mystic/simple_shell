1e4c178e2f75ba2072c68b4766d9d122d0c055ba
Wise-mystic <wisdomkwawuvi23@gmail.com>
2024-01-12 12:01:38 -0800

    shell

---
+++ my_shell.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/*
+ *this is the prompt invoker
+ */
+
+{
+int main(void);
+
+{
+	char command[130];
+
+while (true);
+
+{
+	display_prompt();
+	read_command(command, sizeof(command));
+	execute_command(command);
+}
+}
+	return 0;
+	}
